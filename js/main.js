3cf89d5acd4ef93e9033c4d91ec39156f809d1ad
Abcent1k <snakyizze@gmail.com>
2024-03-20 21:35:53 +0200

    added side menu for phones (no filler), added overlay

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 let lastScrollTop = 0;
-let header = document.querySelector('header');
-let headerHeight = 71;
-let firstSection = document.getElementById("first-section");
+const header = document.querySelector('header');
+const headerHeight = header.offsetHeight;
+const firstSection = document.getElementById("first-section");
+const sidebar = document.getElementById('sidebar');
+const overlay = document.getElementById('overlay');
+
+function getScrollbarWidth() {
+    // Create a block with scrolling
+    let outer = document.createElement("div");
+    outer.style.visibility = "hidden";
+    outer.style.overflow = "scroll"; // make it have scroll bars
+    document.body.appendChild(outer);
+
+    // Create the inner block and add it to the outer block
+    let inner = document.createElement("div");
+    outer.appendChild(inner);
+
+    // Calculate the width of the scroll bar
+    const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+
+    // Removing blocks
+    outer.parentNode.removeChild(outer);
+
+    return scrollbarWidth;
+}
 
 window.addEventListener('scroll', () => {
     let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
@@ -19,3 +41,29 @@ window.addEventListener('scroll', () => {
     }
     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
 }, false);
+
+document.getElementById('menu-icon').addEventListener('click', () => {
+    const scrollbarWidth = getScrollbarWidth();
+    sidebar.classList.toggle('active');
+    overlay.classList.toggle('active');
+
+    if (sidebar.classList.contains('active')) {
+        document.body.style.overflow = 'hidden';
+        document.body.style.paddingRight = scrollbarWidth + 'px';
+        if (header.classList.contains('scrolled'))
+            header.style.width = `calc(100% - ${scrollbarWidth}px)`;
+    } else {
+        document.body.style.overflow = '';
+        document.body.style.paddingRight = '';
+        header.style.width = '100%';
+    }
+});
+
+overlay.addEventListener('click', () => {
+    overlay.classList.remove('active');
+    sidebar.classList.remove('active');
+
+    document.body.style.overflow = '';
+    document.body.style.paddingRight = '';
+    header.style.width = '100%';
+});
